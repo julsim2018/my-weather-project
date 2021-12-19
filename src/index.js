abbd711ab8d370a94d7b93fa1c36186f2eb46aae
julsim2018 <95719458+julsim2018@users.noreply.github.com>
2021-12-19 20:48:38 +0000

    Added relevant icon for current temp

--- src/index.js
+++ src/index.js
@@ -37,6 +37,13 @@ function city(event) {
   axios.get(`${weatherUrl}&appid=${apiKey}`).then(cityWeather);
 
   function cityWeather(response) {
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+    console.log(response.data);
     let name = document.querySelector("#current-location");
     name.innerHTML = response.data.name;
     let temp = document.querySelector("#current-temp");
