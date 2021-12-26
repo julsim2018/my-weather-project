7090d3e34cda2ad6d7ae250bd94ec48bc0e875ce
julsim2018 <95719458+julsim2018@users.noreply.github.com>
2021-12-26 20:53:52 +0000

    integrated forecast API, styled data to fit app

--- src/index.js
+++ src/index.js
@@ -24,25 +24,41 @@ if (minutes < 10) {
   time.innerHTML = `${hours}:${minutes}`;
 }
 ///
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-          <i class="fas fa-cloud"></i>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img id="forecast-icon" src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"/>
           <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">18˚</span>
-            <span class="weather-forecast-temperature-min">12˚</span>
+            <span class="weather-forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}˚</span>
+            <span class="weather-forecast-temperature-min">${Math.round(
+              forecastDay.temp.min
+            )}˚</span>
         </div>
       </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
